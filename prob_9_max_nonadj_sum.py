f6dc553c804c46f521164e8c0632a2c2ffd50606
Yixuan Zhou <thomaszhou2333@gmail.com>
2021-03-11 13:29:01 -0800

    prob 9 faster

--- prob_9_max_nonadj_sum.py
+++ prob_9_max_nonadj_sum.py
@@ -12,6 +12,9 @@ Follow-up: Can you do this in O(N) time and constant space?
 '''
 
 
+'''
+This is 2n
+'''
 def max_non_adj_sum(num_list):
     
     # Sep by 1
@@ -29,6 +32,22 @@ def max_non_adj_sum(num_list):
     ret = max(one_max, two_max)
     return ret
 
+'''
+This is n, tho both O(n), but this is better.
+'''
+def max_non_adj_sum_faster(num_list):
+    
+    twos = [0, 0, 0]
+    ones = [0, 0]
+    for i, num in enumerate(num_list):
+        ones[i%2] += num
+        twos[i%3] += num
+    
+    one_max = max(ones)
+    two_max = max(twos)
+    ret = max(one_max, two_max)
+    return ret
+
 
 if __name__ == '__main__':
     assert max_non_adj_sum([2, 4, 6, 2, 5]) == 13
@@ -36,3 +55,11 @@ if __name__ == '__main__':
     assert max_non_adj_sum([5, 1, 2, 1, 5]) == 12
     assert max_non_adj_sum([]) == 0
     assert max_non_adj_sum([1]) == 1
+    assert max_non_adj_sum([1,2]) == 2
+
+    assert max_non_adj_sum_faster([2, 4, 6, 2, 5]) == 13
+    assert max_non_adj_sum_faster([5, 1, 1, 5]) == 10
+    assert max_non_adj_sum_faster([5, 1, 2, 1, 5]) == 12
+    assert max_non_adj_sum_faster([]) == 0
+    assert max_non_adj_sum_faster([1]) == 1
+    assert max_non_adj_sum_faster([1,2]) == 2
